daa298535a5487abb341853e9755110c29bc6619
vaibhav dinkar <vaibhavdinkar2536@gmail.com>
2023-10-17 14:49:12 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const { MongoClient } = require("mongodb")
+
+const client = new MongoClient(process.env.CONNECTIONSTRING)
+
+async function start() {
+  await client.connect()
+  module.exports = client
+  const app = require("./app")
+  app.listen(process.env.PORT, ()=>{
+    console.log("there was an error")
+  })
+}
+
+start()
